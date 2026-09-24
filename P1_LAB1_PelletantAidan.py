5e71775a408ae3a9c48d04f2e5d38be8b3df908b
AidanPelletant <70720853+AidanPelletant@users.noreply.github.com>
2026-09-24 14:12:42 -0400

    Add files via upload

---
+++ P1_LAB1_PelletantAidan.py
@@ -0,0 +1,13 @@
+# Aidan Pelletant
+# September 24, 2026
+# P1LAB1
+# A Python program that prompts the user for their first and last name and displays a welcome message.
+
+# Prompt the user for their first name
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Enter your last name: ")
+
+# Print the welcome message
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
